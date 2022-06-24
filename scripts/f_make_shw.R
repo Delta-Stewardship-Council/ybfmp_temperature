ebf2bb67a2e5b0ab9b3e035c6dec3821a9ab5ea1
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-06-23 17:17:03 -0700

    final daily time series for sherwood harbor water temperature

--- scripts/f_make_shw.R
+++ scripts/f_make_shw.R
@@ -1,4 +1,6 @@
 # make Sherwood Harbor logger data from YBFMP daily and fill in missing data
+# impute NAs within target timeline (to make continuous daily water temperature data) - 1998-01-16 to 2019-12-31
+# if gap in consecutive days is greater than 7, use lm with Rio Vista
 
 # library
 library(dplyr)
@@ -34,8 +36,83 @@ f_make_shw <- function() {
     shw_daily$max <- ifelse(shw_daily$method == "daily_mean", "NA", shw_daily$max)
     shw_daily$min <- ifelse(shw_daily$method == "daily_mean", "NA", shw_daily$min)
 
-    write.csv(shw_daily, "SHWharbor_98_20_daily_logger.csv", row.names = FALSE)
+    write.csv(shw_daily, "data_clean/SHWharbor_98_20_daily_logger.csv", row.names = FALSE)
 
-    #
+    # impute NAs within seven days of consecutive NAs
+    continous_dates <- data.frame (x = 1:8020, date = seq(as.Date('1998-01-16'),as.Date('2019-12-31'),by='day')) # missing 397 days in 1998
+    shw_daily$date <- as.Date(shw_daily$date)
+    shw_cont <- merge(shw_daily, continous_dates, by = "date", all.y = TRUE)
 
+    # separate
+    shw_cont_data <- shw_cont[!is.na(shw_cont$mean),]
+    shw_cont_NA <- shw_cont[is.na(shw_cont$mean),]
+    head(shw_cont_NA) # 2522 days
+
+    # assigns id to consecutive date groups
+    shw_cont_NA$group <- cumsum(c(1, diff.Date(shw_cont_NA$date)) >= 2)
+
+    shw_cont_NA_sum <- shw_cont_NA %>%
+      group_by(group) %>%
+      summarise(length = length(group)) %>%
+      as.data.frame(shw_cont_NA_sum)
+
+    shw_cont_NA_complete <- merge(shw_cont_NA, shw_cont_NA_sum, by="group", all.x = TRUE)
+    shw_cont_NA_complete <- transform(shw_cont_NA_complete, category = ifelse(length > 7, "Over7", "7&Under"))
+    shw_cont_NA_complete <- transform(shw_cont_NA_complete, method = ifelse(length <= 7, "imputeTS", "lm_rv"))
+    head(shw_cont_NA_complete)
+
+    shw_cont_data$length = NA
+    shw_cont_data$group = NA
+
+    imput_shw <- rbind(shw_cont_NA_complete, shw_cont_data)
+    imput_shw <- imput_shw[order(imput_shw$date),]
+    imput_shw$max <- as.numeric(imput_shw$max)
+    imput_shw$min <- as.numeric(imput_shw$min)
+
+    # 7 day moving average
+    imput_shw$mean <- na_ma(imput_shw$mean, k = 7, weighting = "exponential", maxgap = Inf)
+    imput_shw$max <- na_ma(imput_shw$max, k = 7, weighting = "exponential", maxgap = Inf)
+    imput_shw$min <- na_ma(imput_shw$min, k = 7, weighting = "exponential", maxgap = Inf)
+
+    # need to remove data that is not within parameters
+    imput_shw$mean <- ifelse(imput_shw$category == "Over7", NA, imput_shw$mean)
+    imput_shw$max <- ifelse(imput_shw$category == "Over7" | imput_shw$date <= as.Date("1999-01-13"), NA, imput_shw$max)
+    imput_shw$min <- ifelse(imput_shw$category == "Over7" | imput_shw$date <= as.Date("1999-01-13"), NA, imput_shw$min)# first year is all daily means data
+
+    # make data for lm
+    input <- imput_shw[,c(2:5)]
+    colnames(input) <- c("date", "shw_mean", "shw_max", "shw_min")
+    rv$Date <- as.Date(rv$Date)
+    colnames(rv)[2] <- "date"
+    dat4model <- merge(input, rv[,c(2:5)], by = "date", all.x = TRUE)
+    dat4model_na <- na.omit(dat4model)
+
+    # lm
+    fit_mean <- lm(shw_mean~mean, data = dat4model_na)# Adjusted R-squared:  0.9562
+    fit_max <- lm(shw_max~max, data = dat4model_na)# Adjusted R-squared: 0.9497
+    fit_min <- lm(shw_min~min, data = dat4model_na)# Adjusted R-squared:  0.9544
+
+    df_fill <- dat4model %>%
+      mutate(pred_mean = predict(fit_mean, .), pred_max = predict(fit_max, .), pred_min = predict(fit_min, .))
+
+    # check
+    plot(df_fill$shw_mean, df_fill$pred_mean)
+    plot(df_fill$shw_max, df_fill$pred_max)
+    plot(df_fill$shw_min, df_fill$pred_min) # cluster where shw is higher than predicted, ybfmp team should investigate further when raw data is evaluated
+
+    # add predictions
+    imput_dat_Over7 <- merge(imput_shw, df_fill[,c(1,8:10)], by = "date", all = TRUE)
+    imput_dat_Over7$mean <- ifelse(is.na(imput_dat_Over7$mean), imput_dat_Over7$pred_mean, imput_dat_Over7$mean)
+    imput_dat_Over7$max <- ifelse(is.na(imput_dat_Over7$max), imput_dat_Over7$pred_max, imput_dat_Over7$max)
+    imput_dat_Over7$min <- ifelse(is.na(imput_dat_Over7$min), imput_dat_Over7$pred_min, imput_dat_Over7$min)
+
+    imput_dat_Over7 <- imput_dat_Over7[,-c(12,14:16)]
+
+    # cant fill NAs greater than seven before 1999-02-17 (no rv data either)
+    imput_dat_Over7$method <- ifelse(is.na(imput_dat_Over7$mean) == TRUE,
+                                     NA, imput_dat_Over7$method)
+    imput_dat_Over7$category <- ifelse(is.na(imput_dat_Over7$mean) == TRUE,
+                                     NA, imput_dat_Over7$category)
+
+    write.csv(imput_dat_Over7, "data_clean/clean_shw.csv", row.names = FALSE)
 }
