391be0b70cc98cd923dd570e04940f76e3985bb4
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-07-01 15:42:55 -0700

    combine wq and rstr logger daily data

--- scripts/f_make_yb.R
+++ scripts/f_make_yb.R
@@ -2,44 +2,36 @@
 
 # library
 
-# data
-rstr <- read.csv("data_clean/rstr_98_18_daily_logger.csv")
-wq <- read.csv("data_clean/WQ_w_fish_daily.csv")
-lis <- read.csv("data_clean/clean_lis.csv") # lisbon weir
-rv <- read.csv("data_clean/clean_rv.csv") # rio vista
+f_get_hourly <- function() {
 
-# combine with temperature logger at screw trap
-head(rstr)
-head(wq)
+  # data
+    rstr <- read.csv("data_clean/rstr_98_18_daily_logger.csv")
+    wq <- read.csv("data_clean/WQ_w_fish_daily.csv")
+    lis <- read.csv("data_clean/clean_lis.csv") # lisbon weir
+    rv <- read.csv("data_clean/clean_rv.csv") # rio vista
 
-yb_master <- rbind(rstr, wq)
+  # combine with temperature logger at screw trap
+    head(rstr)
+    head(wq)
 
-# NAs where some WQ but not temp taken and summer 2014
-temp_98_18_daily_master <- temp_98_18_daily_master[!is.na(temp_98_18_daily_master$mean),]
+    yb_master <- rbind(rstr, wq)
+    yb_master$date <- as.Date(yb_master$date)
 
-write.csv(temp_98_18_daily_master, "temp_98_18_daily_master.csv")
+  # opted to keep logger data when there was a duplicate, substitute wq for NAs only
+  # create columns for IDing duplicates in both directions
+    yb_master$dup1 <- duplicated(yb_master$date)
+    yb_master$dup2 <- duplicated(yb_master$date, fromLast = TRUE)
+    yb_master$dup <- ifelse(yb_master$dup1 == TRUE | yb_master$dup2 == TRUE, 1, 0)
 
-colSums(is.na(temp_98_18_daily_master))
-head(temp_98_18_daily_master)
-min(temp_98_18_daily_master$date)
-max(temp_98_18_daily_master$date)
+  # subset
+    yb_master$drop <- ifelse(yb_master$dup == 1 & yb_master$method == "WQ_w_fish", 1, 0)
 
-sum(duplicated(temp_98_18_daily_master$date))#1138
-temp_dup <- temp_98_18_daily_master[duplicated(temp_98_18_daily_master$date), ]# both logger and fish WQ on the same day
+    yb_master_dup <- subset(yb_master, drop == 0)
 
-## opted to keep logger data when there was a duplicate, this can have a more complex rule set?
-# create columns for IDing duplicates in both directions
-temp_98_18_daily_master$dup1 <- duplicated(temp_98_18_daily_master$date)
-temp_98_18_daily_master$dup2 <- duplicated(temp_98_18_daily_master$date, fromLast = TRUE)
-temp_98_18_daily_master$dup <- ifelse(temp_98_18_daily_master$dup1 == TRUE | temp_98_18_daily_master$dup2 == TRUE, 1, 0)
+    yb_master_dup <- yb_master_dup[,-c(10:14)]
 
-# subset
-temp_98_18_daily_master$drop <- ifelse(temp_98_18_daily_master$dup == 1 & temp_98_18_daily_master$method == "RSTR_logger", 1, 0)
+    write.csv(yb_master_dup, "data_clean/yb_98_18_daily_combo.csv")
 
-temp_98_18_daily_master_dup <- subset(temp_98_18_daily_master, drop == 0)
-
-temp_98_18_daily_master_dup <- temp_98_18_daily_master_dup[,-c(9:13)]
-
-write.csv(temp_98_18_daily_master_dup, "temp_98_18_daily_master_du_removed.csv")
+}
 
 
