32203c1dfbc65d4e3e701a585b044f54c98cb9a6
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-07-01 16:36:43 -0700

    something is not working with the lm_lis code

--- scripts/f_make_yb.R
+++ scripts/f_make_yb.R
@@ -1,6 +1,8 @@
 # make yolo bypass data using the rotary screw trap daily data, water quality collected with fish and a lm with LIS and RV data sets (LIS doesn't go back far enough, but is in the Yolo Bypass, unlike Rio Vista, which is below the confluence with the Sacramento River)
 
 # library
+library(imputeTS)
+library(dplyr)
 
 f_get_hourly <- function() {
 
@@ -32,6 +34,80 @@ f_get_hourly <- function() {
 
     write.csv(yb_master_dup, "data_clean/yb_98_18_daily_combo.csv")
 
-}
+  # impute NAs within seven days of consecutive NAs
+    continous_dates <- data.frame (x = 1:8020, date = seq(as.Date('1998-01-16'),as.Date('2019-12-31'),by='day')) #
+
+    yb_cont <- merge(yb_master_dup, continous_dates, by = "date", all.y = TRUE)
+
+  # separate
+    yb_cont_data <- yb_cont[!is.na(yb_cont$mean),]
+    yb_cont_NA <- yb_cont[is.na(yb_cont$mean),]
+    head(yb_cont_NA) # 2882 days
+
+  # assigns id to consecutive date groups
+    yb_cont_NA$group <- cumsum(c(1, diff.Date(yb_cont_NA$date)) >= 2)
+
+    yb_cont_NA_sum <- yb_cont_NA %>%
+      group_by(group) %>%
+      summarise(length = length(group)) %>%
+      as.data.frame(yb_cont_NA_sum)
+
+    yb_cont_NA_complete <- merge(yb_cont_NA, yb_cont_NA_sum, by="group", all.x = TRUE)
+    yb_cont_NA_complete <- transform(yb_cont_NA_complete, category = ifelse(length > 7, "Over7", "7&Under"))
+
+    head(yb_cont_NA_complete)
+
+    yb_cont_data$length = NA
+    yb_cont_data$group = NA
+
+    imput_yb <- rbind(yb_cont_NA_complete, yb_cont_data)
+    imput_yb <- imput_yb[order(imput_yb$date),]
+
+    # 7 day moving average
+    imput_yb$mean <- na_ma(imput_yb$mean, k = 7, weighting = "exponential", maxgap = Inf)
+    imput_yb$max <- na_ma(imput_yb$max, k = 7, weighting = "exponential", maxgap = Inf)
+    imput_yb$min <- na_ma(imput_yb$min, k = 7, weighting = "exponential", maxgap = Inf)
+
+    imput_yb$method <- ifelse(imput_yb$category == "7&Under", "imputeTS", imput_yb$method)
+
+    # need to remove data that is not within parameters
+    imput_yb$mean <- ifelse(imput_yb$category == "Over7", NA, imput_yb$mean)
+    imput_yb$max <- ifelse(imput_yb$category == "Over7" | imput_yb$method == "daily_mean", NA, imput_yb$max)
+    imput_yb$min <- ifelse(imput_yb$category == "Over7" | imput_yb$method == "daily_mean", NA, imput_yb$min)
+
+    # make data for lm
+    input <- imput_yb[,c(2:5)]
+    colnames(input) <- c("date", "yb_mean", "yb_max", "yb_min")
+
+    colnames(lis)[2] <- "date"
+    lis$date <- as.Date(lis$date)
+    lis_input <- merge(input, lis[,c(2:5)], by = "date", all = TRUE)
+    lis_input <- na.omit(lis_input)
+    min(lis$date)# 2008-07-11
+    max(lis$date)# 2019-12-31
+
+    fit_lis_mean <- lm(yb_mean~mean, data = lis_input)# Adjusted R-squared:  0.7967
+    fit_lis_max <- lm(yb_max~max, data = lis_input)# Adjusted R-squared: 0.7838
+    fit_lis_min <- lm(yb_min~min, data = lis_input)# Adjusted R-squared: 0.7958
+
+    lis_fill <- lis_input %>%
+      mutate(pred_mean = predict(fit_lis_mean, .), pred_max = predict(fit_lis_max, .), pred_min = predict(fit_lis_min, .))
+
+    # check
+    plot(lis_fill$yb_mean, lis_fill$pred_mean)
+    plot(lis_fill$yb_max, lis_fill$pred_max)
+    plot(lis_fill$yb_min, lis_fill$pred_min) # some noise to be expected when flooding occurs
+
+    # add predictions
+    imput_dat_Over7 <- merge(imput_yb, lis_fill[,c(1,8:10)], by = "date", all = TRUE)
+
+    imput_dat_Over7$method <- ifelse(is.na(imput_dat_Over7$mean) & !is.na(imput_dat_Over7$pred_mean), "lm_lis", imput_dat_Over7$method)
+    length(is.na(imput_dat_Over7$mean)) # 8020
+
+    imput_dat_Over7$mean <- ifelse(is.na(imput_dat_Over7$mean), imput_dat_Over7$pred_mean, imput_dat_Over7$mean)
+    imput_dat_Over7$max <- ifelse(is.na(imput_dat_Over7$max), imput_dat_Over7$pred_max, imput_dat_Over7$max)
+    imput_dat_Over7$min <- ifelse(is.na(imput_dat_Over7$min), imput_dat_Over7$pred_min, imput_dat_Over7$min)
+
+    }
 
 
